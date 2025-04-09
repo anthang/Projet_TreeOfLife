b7ea9096cba84d5e963876ab1610d5eefd138006
anthang <116769586+anthang@users.noreply.github.com>
2025-04-09 17:48:06 +0200

    Add files via upload

---
+++ TreeOfLife/TreeOfLife/Models/Data.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace TreeOfLifeVisualization.Models
+{
+    public class Data
+    {
+        public List<Node> Nodes { get; private set; }
+        public List<Link> Links { get; private set; }
+        private readonly string _nodesFilePath;
+        private readonly string _linksFilePath;
+
+        public Data(string nodesFilePath, string linksFilePath)
+        {
+            _nodesFilePath = nodesFilePath;
+            _linksFilePath = linksFilePath;
+            LoadNodes();
+            LoadLinks();
+            BuildHierarchy();
+        }
+
+        private void LoadNodes()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true };
+            using var reader = new StreamReader(_nodesFilePath);
+            using var csv = new CsvReader(reader, config);
+            var records = csv.GetRecords<dynamic>();
+            Nodes = new List<Node>();
+
+            foreach (var record in records)
+            {
+                var node = new Node
+                {
+                    NodeId = int.Parse(record.node_id),
+                    NodeName = record.node_name == "none" ? "" : (string)record.node_name,
+                    ChildNodesCount = int.Parse(record.child_nodes),
+                    IsLeaf = record.leaf_node == "1",
+                    HasTolOrgLink = record.tolorg_link == "1",
+                    IsExtinct = record.extinct == "1",
+                    Confidence = int.Parse(record.confidence),
+                    Phylesis = int.Parse(record.phylesis)
+                };
+
+                Nodes.Add(node);
+            }
+        }
+
+        private void LoadLinks()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true };
+            using var reader = new StreamReader(_linksFilePath);
+            using var csv = new CsvReader(reader, config);
+            var records = csv.GetRecords<dynamic>();
+            Links = new List<Link>();
+
+            foreach (var record in records)
+            {
+                var link = new Link
+                {
+                    SourceNodeId = int.Parse(record.source_node_id),
+                    TargetNodeId = int.Parse(record.target_node_id)
+                };
+                Links.Add(link);
+            }
+        }
+
+        private void BuildHierarchy()
+        {
+            var nodeDict = Nodes.ToDictionary(n => n.NodeId);
+
+            foreach (var link in Links)
+            {
+                if (nodeDict.ContainsKey(link.SourceNodeId) && nodeDict.ContainsKey(link.TargetNodeId))
+                {
+                    var parentNode = nodeDict[link.SourceNodeId];
+                    var childNode = nodeDict[link.TargetNodeId];
+                    childNode.Parent = parentNode;
+                    parentNode.Children.Add(childNode);
+                }
+            }
+        }
+    }
+}
---
+++ TreeOfLife/TreeOfLife/Views/View.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using TreeOfLife.Models;
+using TreeOfLifeVisualization.Controllers;
+using TreeOfLifeVisualization.Models;
+
+namespace TreeOfLifeVisualization.Views
+{
+    public partial class View : Form
+    {
+        private Controller _controller;
+        private NodeReduit NHistoric;  
+
+        public View()
+        {
+            InitializeComponent();
+        }
+
+        private void MainView_Load(object sender, EventArgs e)
+        {
+           
+            NHistoric = new NodeReduit();
+
+            
+            string nodesCsvPath = @"C:\Users\antho\Desktop\2024-2025\Programmation_Interface\2025-projet-G1\treeoflife_nodes_simplified.csv";
+            string linksCsvPath = @"C:\Users\antho\Desktop\2024-2025\Programmation_Interface\2025-projet-G1\treeoflife_links_simplified.csv";
+
+            _controller = new Controller(nodesCsvPath, linksCsvPath);
+
+            BuildTreeView();
+        }
+
+        private void BuildTreeView()
+        {
+            treeView1.Nodes.Clear();
+
+            foreach (var rootNodeModel in _controller.Racine)
+            {
+                var rootTreeNode = CreateTreeNode(rootNodeModel);
+                treeView1.Nodes.Add(rootTreeNode);
+            }
+        }
+
+        private TreeNode CreateTreeNode(Node model, HashSet<int> visited = null)
+        {
+            if (visited == null)
+                visited = new HashSet<int>();
+
+            if (visited.Contains(model.NodeId))
+            {
+                return new TreeNode($"[Cycle détecté: {model.NodeName}]");
+            }
+
+            visited.Add(model.NodeId);
+
+            string text = string.IsNullOrEmpty(model.NodeName)
+                            ? $"Node {model.NodeId}"
+                            : model.NodeName;
+
+            TreeNode treeNode = new TreeNode(text)
+            {
+                Tag = model 
+            };
+
+            foreach (var child in model.Children)
+            {
+                treeNode.Nodes.Add(CreateTreeNode(child, visited));
+            }
+
+            return treeNode;
+        }
+
+        private void treeView1_Select(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node != null && e.Node.Tag is Node model)
+            {
+                string info = $"ID: {model.NodeId}\r\n" +
+                              $"Nom: {model.NodeName}\r\n" +
+                              $"Extinct? {model.IsExtinct}\r\n" +
+                              $"Confidence: {model.Confidence}\r\n" +
+                              $"Phylesis: {model.Phylesis}\r\n";
+                labelInfo.Text = info;
+
+    
+                NHistoric.Add(model);
+
+         
+                listBox1.Items.Insert(0,model.NodeName + ":" + model.NodeId.ToString());
+            }
+        }
+    }
+}
